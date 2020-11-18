57d57e83e3e24f6c7a4630b0f1e66698fd095c47
Matix070 <56588768+Matix070@users.noreply.github.com>
2020-11-18 23:16:50 +0100

    Zaliczenie.4

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -1,9 +1,24 @@
 #pragma once
 
-#include "Stocznia.hpp"
+#include "include/Stocznia.hpp"
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    Stocznia stocznia{};
+    unsigned int suma = 0;
+    unsigned int zaglowce = 0;
+
+    if (towar == 0)
+        return 0;
+    do
+    {
+        Statek* s1 = stocznia();
+        if (dynamic_cast<Zaglowiec*> (s1) != nullptr)
+            zaglowce++;
+
+        int ladunek = s1->transportuj();
+        suma += ladunek;
+        delete s1;
+    } while (suma <= towar);
+    
 }
